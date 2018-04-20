17c1469a261d910828771f8ee9b316860988bace
Armando Ortiz <aortiz@sciodev.com>
2018-04-20 10:28:08 -0500

    netcore version

---
+++ SchoolSchedule.API/JwtDecoder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Web.Script.Serialization;
+using JWT;
+
+namespace SchoolSchedule.API
+{
+    public class JwtDecoder
+    {
+        /// <summary>
+        /// Get the userid from the token if the token is not expired
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static int? GetUserIdFromToken(string token)
+        {
+            string key = WebConfigurationManager.AppSettings.Get("jwtKey");
+            var jsonSerializer = new JavaScriptSerializer();
+            var decodedToken = JsonWebToken.Decode(token, key);
+            var data = jsonSerializer.Deserialize<Dictionary<string, object>>(decodedToken);
+            object userId, exp;
+            data.TryGetValue("userId", out userId);
+            data.TryGetValue("exp", out exp);
+            var validTo = FromUnixTime(long.Parse(exp.ToString()));
+            if (DateTime.Compare(validTo, DateTime.UtcNow) <= 0)
+            {
+                return null;
+            }
+            return (int)userId;
+        }
+
+        private static DateTime FromUnixTime(long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+    }
+}
---
+++ SchoolSchedule.API/JwtManager.cs
@@ -0,0 +1,51 @@
+﻿using SchoolSchedule.DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Collections.Generic;
+using System.Web.Configuration;
+using JWT;
+
+namespace SchoolSchedule.API
+{
+    public class JwtManager
+    {
+        
+
+        /// <summary>
+        /// Create a Jwt with user information
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="dbUser"></param>
+        /// <returns></returns>
+        public static string CreateToken(User user, out object dbUser)
+        {
+
+            var unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var expiry = Math.Round((DateTime.UtcNow.AddMinutes(45) - unixEpoch).TotalSeconds);
+            var issuedAt = Math.Round((DateTime.UtcNow - unixEpoch).TotalSeconds);
+            var notBefore = Math.Round((DateTime.UtcNow.AddMonths(6) - unixEpoch).TotalSeconds);
+
+
+            var payload = new Dictionary<string, object>
+            {
+                {"email", user.Email},
+                {"userId", user.Id},
+                //{"role", user.UserRoles},
+                {"sub", user.Id},
+                {"nbf", notBefore},
+                {"iat", issuedAt},
+                {"exp", expiry}
+            };
+
+            var secret = WebConfigurationManager.AppSettings.Get("jwtKey"); //secret key
+            dbUser = new { user.Email, user.Id };
+            var token = JsonWebToken.Encode(payload, secret, JwtHashAlgorithm.HS256);
+            return token;
+        }
+    }
+
+
+}
